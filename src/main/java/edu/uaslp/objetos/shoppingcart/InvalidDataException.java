689c8282762493f80d110218e1277d8a17fd653d
AngSan22 <jesuszx22@gmail.com>
2022-05-03 09:24:52 -0500

    Exam

---
+++ src/main/java/edu/uaslp/objetos/shoppingcart/InvalidDataException.java
@@ -0,0 +1,9 @@
+package edu.uaslp.objetos.shoppingcart;
+
+public class InvalidDataException extends RuntimeException{
+    public InvalidDataException(String error){
+        super(error);
+    }
+
+
+}
--- src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
+++ src/main/java/edu/uaslp/objetos/shoppingcart/ShoppingCart.java
@@ -1,4 +1,56 @@
 package edu.uaslp.objetos.shoppingcart;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.OptionalDouble;
+
 public class ShoppingCart {
+    List<Item> shoppingCart = new ArrayList<>();
+    int itemsCount = 0;
+    public boolean isEmpty() {
+        if (shoppingCart.size() > 0){
+            return false;
+        }
+        return true;
+    }
+
+
+    public BigDecimal getTotalCost() {
+        if (isEmpty()) {
+            throw new EmptyShoppingCartException();
+        }
+        BigDecimal totalCost = BigDecimal.ZERO;
+
+        for(Item item: shoppingCart){
+            totalCost = totalCost.add(item.getUnitCost().multiply(BigDecimal.valueOf(item.getQuantity())));
+        }
+
+        return totalCost;
+
+    }
+
+    public void addItem(Item item) {
+        for (int i=0; i<shoppingCart.size(); i++){
+            if(shoppingCart.get(i).getCode() == item.getCode() && shoppingCart.get(i).getUnitCost() == item.getUnitCost()){
+                shoppingCart.get(i).setQuantity(shoppingCart.get(i).getQuantity() + item.getQuantity());
+            }
+        }
+        shoppingCart.add(item);
+    }
+
+    public int getItemsCount() {
+        for (int i=0; i<shoppingCart.size(); i++){
+            itemsCount = itemsCount + shoppingCart.get(i).getQuantity();
+        }
+        return itemsCount;
+    }
+
+    public List<Item> getItems() {
+        return shoppingCart;
+    }
+
+    public void removeItem(String item) {
+    }
 }
